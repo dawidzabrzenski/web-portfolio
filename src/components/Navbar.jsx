4a50d1046d8da20f33b34f8c7b7f57bea56eeb35
dawidzabrzenski <92026489+dawidzabrzenski@users.noreply.github.com>
2024-10-02 07:47:54 +0200

    dynamic island navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import BurgerIcon from "./BurgerIcon";
+import { motion } from "framer-motion";
 
 function Navbar() {
   const [active, setActive] = useState(false);
@@ -12,11 +13,25 @@ function Navbar() {
         </h2>
         <BurgerIcon value={active} setValue={setActive} />
       </div>
-      {/* <ul className="mt-4 flex justify-center gap-4 rounded-full bg-stone-800 py-3">
-        <li>Home</li>
-        <li>Test</li>
-        <li>Test3</li>
-      </ul> */}
+
+      {active && (
+        <div className="flex justify-center">
+          <motion.ul
+            initial={{ opacity: 0, y: -80, width: "66%" }}
+            animate={[{ opacity: 1, y: 0 }, { width: "100vw" }]}
+            transition={{
+              opacity: { duration: 0.2 },
+              y: { duration: 0.2 },
+              width: { duration: 0.3, delay: 0.3 },
+            }}
+            className="mt-4 flex justify-center gap-6 rounded-full bg-stone-800 py-3"
+          >
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </motion.ul>
+        </div>
+      )}
     </nav>
   );
 }
