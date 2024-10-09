f6d75e32339ef892f8b20589d1699a58a3702f16
dawidzabrzenski <92026489+dawidzabrzenski@users.noreply.github.com>
2024-10-09 14:17:33 +0200

    Testing form email sending

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import AboutMe from "./ui/AboutMe";
 import Projects from "./ui/Projects";
 import Contact from "./ui/Contact";
 import Navbar from "./components/Navbar";
+import Form from "./ui/Form";
+
+// snap-y snap-mandatory
 
 function App() {
   return (
-    <div className="h-screen snap-y snap-mandatory overflow-x-hidden selection:bg-green-300">
+    <div className="h-screen overflow-x-hidden selection:bg-green-300">
       <Navbar />
       <div id="home" className="snap-start">
         <Hero />
@@ -20,6 +23,7 @@ function App() {
       <div id="contact" className="snap-start">
         <Contact />
       </div>
+      <Form />
     </div>
   );
 }
