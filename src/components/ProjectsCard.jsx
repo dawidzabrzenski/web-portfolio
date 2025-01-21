fbf592a61fb60f2ac20d589d2e46cff0c62e9162
Dawid Zabrzeński <dawid.zabrzenski@gfcorp.pl>
2025-01-21 13:38:11 +0100

    chore: project card styling changes for tablet

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -12,7 +12,7 @@ ProjectsCard.propTypes = {
 
 const WorkInProgressBadge = () => (
   <p className="rounded-xl bg-orange-500 px-2 py-1 text-xs font-light transition-colors duration-300 hover:bg-orange-600 sm:text-sm">
-    WORK IN PROGRESS
+    IN DEV
   </p>
 );
 
@@ -37,17 +37,19 @@ function ProjectsCard({ name, children, href, desc, workInProgress }) {
   );
 
   return (
-    <div className="flex flex-col rounded-lg bg-[#1B1B1B] md:max-w-[80vw] md:flex-row">
+    <div className="flex flex-col rounded-lg bg-[#1B1B1B] md:max-w-[80vw] lg:flex-row">
       <img
         loading="lazy"
-        className="min-h-12 rounded-t-lg object-cover object-left md:max-w-[35vw] md:rounded-l-[0.5rem] md:rounded-r-none"
+        className="min-h-12 rounded-t-lg object-cover object-left md:rounded-l-[0.5rem] md:rounded-r-none lg:max-w-[35vw]"
         src={`./projects/${name.toLowerCase().replace(/\s/g, "")}.webp`}
         alt={`Screenshot from IT project for portfolio called ${name}`}
       />
       <div className="flex flex-col justify-between px-6 py-4">
         <div>
           <div className="mb-2 flex items-center justify-between">
-            <h4 className="text-gradient-sec text-3xl font-medium">{name}</h4>
+            <h4 className="text-gradient-sec text-xl font-medium lg:text-3xl">
+              {name}
+            </h4>
             <div className="flex items-center gap-4">
               {workInProgress && <WorkInProgressBadge />}
               <ProjectLink
